df99470c681863d8f4f457460290b8cfedb6bd00
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-09-04 04:19:31 +0100

    updated models and added relations

--- model/schema.go
+++ model/schema.go
@@ -2,10 +2,19 @@ package model
 
 import "github.com/jinzhu/gorm"
 
+// User struct
+type User struct {
+	gorm.Model
+	Email    string    `gorm:"unique_index;not null" json:"email"`
+	Password string    `gorm:"not null" json:"password"`
+	Sponsors []Sponsor `gorm:"foreignKey:UserID"`
+}
+
 // Sponsor struct
 type Sponsor struct {
 	gorm.Model
 	Name   string `gorm:"not null" json:"name"`
 	Link   string `gorm:"not null" json:"link"`
 	Amount int    `gorm:"not null" json:"amount"`
+	UserID uint
 }
